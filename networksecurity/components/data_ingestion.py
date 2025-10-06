124b3e782db0a188a59ba6a90a63b8648fd560cd
Anubhav127 <anu.verma712@gmail.com>
2025-10-07 04:24:01 +0530

    Pipeline is completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -20,16 +20,16 @@ MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 
 
 class DataIngestion:
-    def __init__(self, data_ingsetion_config: DataIngestionConfig):
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
         try:
-            self.data_ingsetion_config = data_ingsetion_config
+            self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
     def export_collection_as_dataframe(self):
         try:
-            database_name = self.data_ingsetion_config.database_name
-            collection_name = self.data_ingsetion_config.collection_name
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingestion_config.collection_name
             self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
             collection = self.mongo_client[database_name][collection_name]
 
@@ -50,8 +50,8 @@ class DataIngestion:
             self.split_data_as_train_test(dataframe)
 
             dataingestionartifact = DataIngestionArtifact(
-                trained_file_path=self.data_ingsetion_config.training_file_path,
-                test_file_path=self.data_ingsetion_config.testing_file_path,
+                trained_file_path=self.data_ingestion_config.training_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path,
             )
 
             return dataingestionartifact
@@ -61,11 +61,11 @@ class DataIngestion:
 
     def export_data_to_feature_store(self, dataframe: pd.DataFrame):
         try:
-            featue_store_file_path = self.data_ingsetion_config.feature_store_file_path
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
             # creating folder
-            dir_path = os.path.dirname(featue_store_file_path)
+            dir_path = os.path.dirname(feature_store_file_path)
             os.makedirs(dir_path, exist_ok=True)
-            dataframe.to_csv(featue_store_file_path, index=False, header=True)
+            dataframe.to_csv(feature_store_file_path, index=False, header=True)
             return dataframe
 
         except Exception as e:
@@ -74,7 +74,7 @@ class DataIngestion:
     def split_data_as_train_test(self, dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
-                dataframe, test_size=self.data_ingsetion_config.train_test_split_ratio
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
             )
 
             logging.info("Performed train test split on the dataframe")
@@ -82,16 +82,16 @@ class DataIngestion:
                 "Exited split_data_as_train_test method of Data ingestion class"
             )
 
-            dir_path = os.path.dirname(self.data_ingsetion_config.training_file_path)
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
             os.makedirs(dir_path, exist_ok=True)
 
             logging.info("Exporting train ans test file path")
 
             train_set.to_csv(
-                self.data_ingsetion_config.training_file_path, index=False, header=True
+                self.data_ingestion_config.training_file_path, index=False, header=True
             )
             test_set.to_csv(
-                self.data_ingsetion_config.testing_file_path, index=False, header=True
+                self.data_ingestion_config.testing_file_path, index=False, header=True
             )
 
             logging.info("Exported train and test file path")
