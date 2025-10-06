33ed02c91d853b238b36694129c15015ac138054
Anubhav127 <anu.verma712@gmail.com>
2025-10-07 03:41:35 +0530

    Model Trainer Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -5,17 +5,22 @@ import pandas as pd
 
 
 """
-DEFINING common constant variable for training pipeline 
+defining common constant variable for training pipeline
 """
 TARGET_COLUMN = "Result"
-PIPELINE_NAME:str = "NetworkSecurity"
+PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
-FILE_NAME: str = "phishingData.csv"
+FILE_NAME: str = "phisingData.csv"
 
-TRAIN_FILE_NAME = "train.csv"
-TEST_FILE_NAME = "test.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
+
 
 
 """
